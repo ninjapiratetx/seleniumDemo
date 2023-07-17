e4a5ab6c56cc6de2df9ffc547127521e5c5bb943
ninjapiratetx <ninjapiratetx@gmail.com>
2023-07-17 00:57:15 -0500

    moving some code around

--- demo/src/test/java/testSel.java
+++ demo/src/test/java/testSel.java
@@ -3,15 +3,20 @@
 import org.junit.After;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class testSel {
     WebDriver driver;
-    @Test
-    public void test() {
+    @Before
+    public void beforeTest() {
         String setProperty = System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
+    }
+    
+    @Test
+    public void test() {
         googleMainPage mainPage = new googleMainPage(driver);
         mainPage.goToGoogleMain();
         Assert.assertEquals("Google", mainPage.getTitle());
